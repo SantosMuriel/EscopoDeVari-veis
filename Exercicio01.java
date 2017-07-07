9023992472e8821e55ea4f0682603add633d52e1
Muriel Santos <muriel.sena@gmail.com>
2017-07-07 17:35:02 -0300

    Percorrer o intervalo de 0 à 100 e imprimir quantos números ímpares exixtem entre 10 e 50

--- Exercicio01.java
+++ Exercicio01.java
@@ -1,9 +1,11 @@
 public class Exercicio01 {
 	public static void main(String[] args) {
+		int aux = 0;
 		for (int x = 0; x <= 100; x++) {
-			if ((x % 2 == 0 ) && (x > 0)) {
-				System.out.println(x);
+			if ((x % 2 != 0 ) && (x > 10 && x < 50)) {
+				aux++;
 			}
-		}	
+		}
+		System.out.println(aux);	
 	}	
 }
