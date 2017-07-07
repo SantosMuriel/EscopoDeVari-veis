a3f9794b4a2c7db6fbcc2ebd1664bcf1690ffaa2
Muriel Santos <muriel.sena@gmail.com>
2017-07-07 17:17:35 -0300

    1 - percorrer o intervalo de 0 à 180 e imprimir somente o intervalo entre 30 e 180

---
+++ Exercicio01.java
@@ -0,0 +1,9 @@
+public class Exercicio01 {
+	public static void main(String[] args) {
+		for (int x = 0; x <= 180; x++) {
+			if ((x >= 30) && (x <= 180)) {
+				System.out.println(x);
+			}
+		}	
+	}	
+}
